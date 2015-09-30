f007825357b4849e5ecdbf456acc3fc2069cab92
Jeremy McDermond (NH6Z) <nh6z@nh6z.net>
2015-09-30 13:28:43 -0700

    Sort destination links
    Destinations need to be sorted in the autocomplete code.  Fixes #35.

--- Buster/BTRAddPopupController.m
+++ Buster/BTRAddPopupController.m
@@ -13,22 +13,15 @@
 @interface BTRAddPopupController ()
 
 @property (nonatomic, getter=inAutoComplete) BOOL complete;
-@property (nonatomic) NSRange lastCompleteRange;
 
 @end
 
 @implementation BTRAddPopupController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do view setup here.
-}
-
 - (IBAction)doAdd:(id)sender {
     NSString *linkTarget = [self.destinationField.stringValue stringByPaddingToLength:8 withString:@" " startingAtIndex:0];
     linkTarget = [linkTarget stringByReplacingCharactersInRange:NSMakeRange(7, 1) withString:self.moduleField.titleOfSelectedItem];
     
-    NSLog(@"String is %@", linkTarget);
     [self.reflectorArrayController addObject:@{ @"reflector": linkTarget } ];
     
     self.destinationField.stringValue = @"";
@@ -36,20 +29,17 @@
 }
 
 -(NSArray<NSString *> *) control:(NSControl *)control textView:(nonnull NSTextView *)textView completions:(nonnull NSArray<NSString *> *)words forPartialWordRange:(NSRange)charRange indexOfSelectedItem:(nonnull NSInteger *)index {
-    NSLog(@"In completion handler");
     
     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF like[cd] %@", [[textView.string copy] stringByAppendingString:@"*"]];
     
-    NSArray<NSString *> *destinations = [BTRDataEngine sharedInstance].linkDriverDestinations;
-    NSArray<NSString *> *completionStrings = [destinations filteredArrayUsingPredicate:predicate];
+    NSArray<NSString *> *completionStrings = [[BTRDataEngine sharedInstance].linkDriverDestinations filteredArrayUsingPredicate:predicate];
+    completionStrings = [completionStrings sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
     
     *index = -1;
     return completionStrings;
 }
 
 -(void)controlTextDidChange:(NSNotification *)obj {
-    NSLog(@"Text changed: %@", ((NSTextView *)obj.userInfo[@"NSFieldEditor"]).string);
-
     if(self.inAutoComplete)
         return;
     
@@ -58,17 +48,4 @@
     self.complete = NO;
 }
 
-/* -(BOOL)control:(NSControl *)control textView:(NSTextView *)textView doCommandBySelector:(SEL)commandSelector {
-    if(commandSelector == @selector(deleteBackward:)) {
-        if(textView.string.length < 1)
-            return NO;
-        
-        self.complete = YES;
-        textView.string = [textView.string substringToIndex:textView.string.length - 1];
-        self.complete = NO;
-        return NO;
-    }
-    
-    return NO;
-} */
 @end
