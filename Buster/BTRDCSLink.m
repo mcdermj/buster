b180faa273a9c4ff62188730fe72f83bd000082c
Jeremy McDermond <nh6z@nh6z.net>
2015-10-05 18:34:27 -0700

    DCS Receive code
    Initial cut at DCS receive.  Need to make sure that Extra also works
    okay after changes to BTRLinkDriver.m

--- Buster/BTRDCSLink.m
+++ Buster/BTRDCSLink.m
@@ -13,6 +13,29 @@
 #import "DStarUtils.h"
 
 #pragma pack(push, 1)
+struct dcs_frame {
+    char magic[4];
+    char flags[3];
+    char rpt2Call[8];
+    char rpt1Call[8];
+    char urCall[8];
+    char myCall[8];
+    char myCall2[4];
+    unsigned short id;
+    char sequence;
+    char voice[9];
+    char data[3];
+    char repeaterSequence[3];
+    char unknown[3];
+    char text[36];
+};
+
+struct dcs_poll {
+    char callsign[8];
+    char null;
+    char reflectorCall[8];
+};
+
 struct dcs_packet {
     union {
         struct {
@@ -31,14 +54,15 @@ struct dcs_packet {
                 } request;
             };
         } link;
-        struct dstar_frame frame;
+        struct dcs_frame frame;
     };
 };
+
 #pragma pack(pop)
 
 static const struct dcs_packet linkTemplate = {
     .link.callsign = "        ",
-    .link.module = 'A',
+    .link.module = 'D',
     .link.reflectorModule = ' ',
     .link.request.terminator = 0x00,
     .link.request.reflectorCall = "        ",
@@ -50,6 +74,8 @@ static NSDictionary *_reflectorList;
 @interface BTRDCSLink ()
 
 +(NSDictionary *)reflectorList;
+
+-(void)processFrame:(struct dcs_frame *)frame;
 
 @end
 
@@ -68,7 +94,7 @@ static NSDictionary *_reflectorList;
 }
 
 -(unsigned short)clientPort {
-    return 30051;
+    return 0;
 }
 -(unsigned short)serverPort {
     return 30051;
@@ -100,13 +126,150 @@ static NSDictionary *_reflectorList;
     return [[BTRDCSLink reflectorList].allKeys copy];
 }
 
--(void)processPacket:(NSData *)packet {
-    NSLog(@"Got packet %@", packet);
+-(void)processPacket:(NSData *)packetData {
+    struct dcs_packet *packet = (struct dcs_packet *) packetData.bytes;
+    
+    if(!memcmp(packet, "0001", 4)) {
+        [self processFrame:&packet->frame];
+        return;
+    }
+    
+    if(!memcmp(packet, "EEEE", 4)) {
+        NSLog(@"Got status packet");
+        return;
+    }
+    
+    switch(packetData.length) {
+        case 17:
+        case 22:
+            //  Poll packet.  Ignore because this is handled in the superclass.
+            break;
+        case 14:
+        case 19:
+        case 519:
+            // Link acknowledgment packet
+            if(!strncmp(packet->link.response.response, "ACK", 3)) {
+                self.linkState = LINKED;
+                NSLog(@"Got ACK, going linked");
+            } else if(!strncmp(packet->link.response.response, "NAK", 3)) {
+                NSLog(@"Got NAK, unlinking");
+                NSError *error = [NSError errorWithDomain:@"BTRErrorDomain" code:3 userInfo:@{ NSLocalizedDescriptionKey : [NSString stringWithFormat:@"%@ refused the link request", self.linkTarget]}];
+                [self.delegate destinationDidError:self.linkTarget error:error];
+                [self unlink];
+            } else
+                NSLog(@"Unknown link acknowledgment");
+            break;
+        case 35:
+            //  Status data
+            break;
+        default:
+            NSLog(@"Unknown link packet");
+            break;
+    }
 }
+
+-(void)processFrame:(struct dcs_frame *)frame {
+    BTRLinkDriver __weak *weakSelf = self;
+    
+    //  Ignore packets not in our current stream
+    if(self.rxStreamId && self.rxStreamId != frame->id)
+        return;
+    
+    if(!self.rxStreamId) {
+        //  XXX There can be null values here!
+        NSDictionary *header = @{
+                                 @"rpt1Call" : [NSString stringWithCallsign:frame->rpt1Call],
+                                 @"rpt2Call" : [NSString stringWithCallsign:frame->rpt2Call],
+                                 @"myCall" : [NSString stringWithCallsign:frame->myCall],
+                                 @"myCall2" : [NSString stringWithShortCallsign:frame->myCall2],
+                                 @"urCall" : [NSString stringWithCallsign:frame->urCall],
+                                 @"streamId" : [NSNumber numberWithUnsignedInteger:frame->id],
+                                 @"time" : [NSDate date],
+                                 @"message" : @""
+                                 };
+        
+        NSLog(@"New stream %@", header);
+        self.rxStreamId = frame->id;
+        [self.delegate streamDidStart:header];
+        self.qsoTimer = [[BTRNetworkTimer alloc] initWithTimeout:5.0 failureHandler: ^{
+            [weakSelf terminateCurrentStream];
+        }];
+    }
+ 
+    [self.qsoTimer ping];
+    
+    //  XXX This should be using a local variable set by the DataEngine.
+    [self.delegate addData:(void *)frame->data streamId:self.rxStreamId];
+            
+    //  If the 0x40 bit of the sequence is set, this is the last packet of the stream.
+    if(frame->sequence & 0x40) {
+        [self terminateCurrentStream];
+        frame->sequence &= ~0x40;
+    }
+            
+    if(frame->sequence != self.rxSequence) {
+        //  If the packet is more recent, reset the sequence, if not, wait for my next packet
+        if(isSequenceAhead(frame->sequence, self.rxSequence, 21)) {
+            NSLog(@"Skipped packet: incoming %u, sequence = %u",frame->sequence, self.rxSequence);
+            self.rxSequence = frame->sequence;
+        } else {
+            NSLog(@"Out of order packet: incoming = %u, sequence = %u\n", frame->sequence, self.rxSequence);
+            return;
+        }
+    }
+            
+    if(self.rxStreamId == 0)
+        self.rxSequence = 0;
+    else
+        self.rxSequence = (self.rxSequence + 1) % 21;
+            
+    //  If streamId == 0, we are on the last packet of this stream.
+    [self.vocoder decodeData:frame->voice lastPacket:(self.rxStreamId == 0)];
+}
+
+
 -(void)sendPoll {
-    [self doesNotRecognizeSelector:_cmd];
+    NSMutableData *pollPacket = [NSMutableData dataWithLength:sizeof(struct dcs_poll)];
+    struct dcs_poll *poll = pollPacket.mutableBytes;
+    
+    [self.rpt1Call getBytes:poll->callsign maxLength:sizeof(poll->callsign) usedLength:NULL encoding:NSASCIIStringEncoding options:0 range:NSMakeRange(0, sizeof(poll->callsign)) remainingRange:NULL];
+    [self.linkTarget getBytes:poll->reflectorCall maxLength:sizeof(poll->reflectorCall) usedLength:NULL encoding:NSASCIIStringEncoding options:0 range:NSMakeRange(0, sizeof(poll->reflectorCall)) remainingRange:NULL];
+    poll->null = 0x00;
+    
+    NSLog(@"Sending poll %@", pollPacket);
+    
+    [self sendPacket:pollPacket];
 }
--(void)sendLinkPacketWithModule:(char)module {
+
+
+-(void)fillLinkPacket:(struct dcs_packet *)packet {
+    //  XXX This works but is ugly.  characterAtIndex returns an unichar, and we should really
+    //  XXX do some sort of real UTF8 conversion rather than just casting it down.
+    //  XXX Maybe it doesn't make much of a difference since the module should only be
+    //  XXX in the range of A-Z.
+    NSAssert(self.rpt1Call.length == 8, @"rpt1Call is not 8 characters");
+    packet->link.module = (char) [self.rpt1Call characterAtIndex:7];
+    [self.rpt1Call getBytes:packet->link.callsign maxLength:sizeof(packet->link.callsign) usedLength:NULL encoding:NSASCIIStringEncoding options:0 range:NSMakeRange(0, 8) remainingRange:NULL];
+    [self.linkTarget.paddedCall getBytes:packet->link.request.reflectorCall maxLength:sizeof(packet->link.request.reflectorCall) usedLength:NULL encoding:NSASCIIStringEncoding options:0 range:NSMakeRange(0, 7) remainingRange:NULL];
+}
+
+-(void)sendUnlink {
+    if([self.linkTarget isEqualToString:@""])
+        return;
+
+    NSMutableData *linkPacket = [NSMutableData dataWithBytes:&linkTemplate length:19];  //  XXX Can we get rid of the magic size?
+    struct dcs_packet *packet = (struct dcs_packet *)linkPacket.mutableBytes;
+
+    [self fillLinkPacket:packet];
+    packet->link.reflectorModule = ' ';
+    [self sendPacket:linkPacket];
+
+    self.linkState = UNLINKED;
+}
+
+-(void)sendLink {
+    NSLog(@"Sending link packet");
+    
     if([self.linkTarget isEqualToString:@""])
         return;
     
@@ -115,46 +278,14 @@ static NSDictionary *_reflectorList;
                           [[NSBundle mainBundle] infoDictionary][@"CFBundleVersion"],
                           [[NSBundle mainBundle] infoDictionary][@"CFBundleShortVersionString"]];
     
-    NSLog("HTML is %@", infoHtml);
-    NSLog("Structure is %ld bytes", sizeof(struct dcs_packet));
-    
-    NSMutableData *linkPacket = nil;
-    if(module == ' ') {
-        linkPacket = [NSMutableData dataWithBytes:&linkTemplate length:19];
-    } else {
-        linkPacket = [NSMutableData dataWithBytes:&linkTemplate length:519];
-        //  XXX Copy the HTML data here.
-    }
-    
+    NSMutableData *linkPacket = [NSMutableData dataWithBytes:&linkTemplate length:519];  //  XXX Can we get rid of the magic size?
     struct dcs_packet *packet = (struct dcs_packet *)linkPacket.mutableBytes;
     
-    //  XXX This works but is ugly.  characterAtIndex returns an unichar, and we should really
-    //  XXX do some sort of real UTF8 conversion rather than just casting it down.
-    //  XXX Maybe it doesn't make much of a difference since the module should only be
-    //  XXX in the range of A-Z.
-    NSAssert(self.rpt1Call.length == 8, @"rpt1Call is not 8 characters");
-    packet->link.module = (char) [self.rpt1Call characterAtIndex:7];
-    packet->link.reflectorModule = module;
-    [self.rpt1Call getBytes:packet->link.callsign maxLength:sizeof(packet->link.callsign) usedLength:NULL encoding:NSASCIIStringEncoding options:0 range:NSMakeRange(0, 8) remainingRange:NULL];
-    [self.linkTarget.paddedCall getBytes:packet->link.request.reflectorCall maxLength:sizeof(packet->link.request.reflectorCall) usedLength:NULL encoding:NSASCIIStringEncoding options:0 range:NSMakeRange(0, 7) remainingRange:NULL];
-    
+    [self fillLinkPacket:packet];
+    packet->link.reflectorModule = (char) [self.linkTarget.paddedCall characterAtIndex:7];
+    [infoHtml getBytes:packet->link.request.htmlData maxLength:sizeof(packet->link.request.htmlData) usedLength:NULL encoding:NSASCIIStringEncoding options:0 range:NSMakeRange(0, 500) remainingRange:NULL];
+
     [self sendPacket:linkPacket];
 }
-
--(void)sendUnlink {
-    [self sendLinkPacketWithModule:' '];
-    self.linkState = UNLINKED;
-}
-
--(void)sendLink {
-    NSLog(@"Sending link packet");
-    
-    [self sendLinkPacketWithModule:(char) [self.linkTarget.paddedCall characterAtIndex:7]];
-}
-
--(void)sendFrame:(struct dstar_frame *)frame {
-    [self doesNotRecognizeSelector:_cmd];
-}
-
 
 @end
