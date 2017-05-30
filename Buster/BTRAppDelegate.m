07060b16bf5f3ef05e5399c8a6341b9c82a1684b
Jeremy McDermond <nh6z@nh6z.net>
2017-05-29 20:53:18 -0700

    Remove HockeyApp debugging

--- Buster/BTRAppDelegate.m
+++ Buster/BTRAppDelegate.m
@@ -27,8 +27,6 @@
 
 #import "BTRAppDelegate.h"
 
-#import <HockeySDK/HockeySDK.h>
-
 #import "BTRDataEngine.h"
 #import "BTRDV3KSerialVocoder.h"
 #import "BTRSlowDataCoder.h"
@@ -48,10 +46,6 @@
 }
 
 - (void)applicationWillFinishLaunching:(NSNotification *)aNotification {
-    [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:@"cc76112de7b80cf017acd344d6072ec9"];
-    // Do some additional configuration if needed here
-    [[BITHockeyManager sharedHockeyManager] startManager];
-
     BTRDataEngine *engine = [BTRDataEngine sharedInstance];
     [engine.slowData bind:@"message" toObject:[NSUserDefaultsController sharedUserDefaultsController] withKeyPath:@"values.slowDataMessage" options:nil];
     
